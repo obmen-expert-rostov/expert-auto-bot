d6e0f14f515981e19634da0e9c0861197bc83daf
Владислав <ukrall@MacBook-Air-Vladislav.local>
2025-07-07 21:58:14 +0300

    Добавил вывод chat.id

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     await message.answer("Привет! Я бот Expert Автозапчасти. Напиши, что нужно подобрать.")
 
+@dp.message_handler()
+async def debug_chat_id(message: types.Message):
+    await message.reply(f"chat.id = {message.chat.id}")
+
 if __name__ == '__main__':
     executor.start_polling(dp)
