6f62e0adc7a402b78432da28d94bda802892e66c
Владислав <ukrall@MacBook-Air-Vladislav.local>
2025-07-07 19:15:13 +0300

    Добавил main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, types, executor
+import os
+
+API_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Я бот Expert Автозапчасти. Напиши, что нужно подобрать.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
